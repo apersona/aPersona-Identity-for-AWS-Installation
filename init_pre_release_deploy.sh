80b7f3550f4912d03784cc2b0b172e3d70a964bf
rexservices <121511803+rexservices@users.noreply.github.com>
2025-05-04 00:50:45 -0400

    Deleting pre-release deploy script from main branch, added pre-release deploy script to .gitignore

--- init_pre_release_deploy.sh
+++ init_pre_release_deploy.sh
@@ -3,7 +3,7 @@
 #project code repo
 export APERSONAIDP_REPO_NAME=Pre-Release-aPersona-Identity-for-AWS-End-User-Services
 export APERSONAADM_REPO_NAME=Pre-Release-aPersona-Identity-for-AWS-Admin-Portal
-export UPDATE_SCRIPT_NAME="update_pre-release"
+export UPDATE_SCRIPT_NAME="update_pre-release.sh"
 
 echo "About to run deploy steps with APERSONAIDP_REPO_NAME:$APERSONAIDP_REPO_NAME"
 bash run_deploy_steps.sh
